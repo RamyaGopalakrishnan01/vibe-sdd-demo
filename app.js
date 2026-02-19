a9646ab2a6c7a00fa936e509b990da6a9d8fd4cc
Ramya Gopalakrishnan <ramyagopalakrishnan17@gmail.com>
2026-02-19 11:17:20 +0530

    Add registration management functions

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-let tasks = [];
-let idCounter = 1;
+function getRegistrations() {
+    const data = localStorage.getItem("registrations");
+    return data ? JSON.parse(data) : [];
+}
 
-console.log("Task Manager Started");
+function saveRegistrations(registrations) {
+    localStorage.setItem("registrations", JSON.stringify(registrations));
+}
+
+function addRegistration(student) {
+    const registrations = getRegistrations();
+    registrations.push(student);
+    saveRegistrations(registrations);
+}
+
+function removeRegistration(id) {
+    let registrations = getRegistrations();
+    registrations = registrations.filter(student => student.id !== id);
+    saveRegistrations(registrations);
+}
