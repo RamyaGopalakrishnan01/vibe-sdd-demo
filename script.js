ca417b7809130d90e1f7b0c39a6162ef88195dc4
Ramya Gopalakrishnan <ramyagopalakrishnan17@gmail.com>
2026-02-19 11:16:42 +0530

    Add student registration and display functions

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
+function registerStudent() {
+    const name = document.getElementById("name").value;
+    const roll = document.getElementById("roll").value;
+    const event = document.getElementById("event").value;
+
+    if (!name || !roll || !event) {
+        alert("All fields are required!");
+        return;
+    }
+
+    const student = {
+        id: Date.now(),
+        name: name,
+        roll: roll,
+        event: event
+    };
+
+    addRegistration(student);
+
+    document.getElementById("name").value = "";
+    document.getElementById("roll").value = "";
+    document.getElementById("event").value = "";
+
+    displayRegistrations();
+}
+
+function displayRegistrations() {
+    const list = document.getElementById("registrationList");
+    list.innerHTML = "";
+
+    const registrations = getRegistrations();
+
+    registrations.forEach(student => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            ${student.name} | ${student.roll} | ${student.event}
+            <button onclick="cancelRegistration(${student.id})">Cancel</button>
+        `;
+
+        list.appendChild(li);
+    });
+}
+
+function cancelRegistration(id) {
+    removeRegistration(id);
+    displayRegistrations();
+}
+
+window.onload = displayRegistrations;
 
